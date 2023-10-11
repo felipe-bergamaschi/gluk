4a8707932bcbf0950a0f70f3e83b8fa6c778f583
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-11 17:46:50 -0300

    feat: add header

---
+++ src/components/layouts/common/header/index.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <header
+      className="w-100 d-flex align-items-center px-2 bg-body-tertiary"
+      style={{
+        height: 64
+      }}
+    >
+      <h1 className="h3 m-0">
+        Gluk
+      </h1>
+    </header>
+  )
+}
