c66834ebebabce15b412ad8108c22adc1537f483
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-11 17:14:24 -0300

    feat: add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Routes from 'vite-plugin-router'
 
+import path from 'path';
+
 export default defineConfig({
   plugins: [react(), Routes()],
+
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ]
+  }
 })
