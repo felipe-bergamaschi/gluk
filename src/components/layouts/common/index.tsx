16e98d222afe978ff4d1b05d79b9fe05304dd0a9
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-11 18:34:03 -0300

    feat: add title & breadcrumbs

--- src/components/layouts/common/index.tsx
+++ src/components/layouts/common/index.tsx
@@ -2,10 +2,12 @@ import { Header } from "./header"
 import { Sidebar } from "./sidebar"
 
 interface CommonLayoutProps {
+  title?: string;
+  breadcrumbs?: string[];
   children: React.ReactNode
 }
 
-export function CommonLayout({ children }: CommonLayoutProps) {
+export function CommonLayout({ children, title, breadcrumbs }: CommonLayoutProps) {
   return (
     <div className="w-100 h-100 overflow-hidden d-flex">
       <Sidebar />
@@ -14,6 +16,22 @@ export function CommonLayout({ children }: CommonLayoutProps) {
         <Header />
 
         <div className="bg-body flex-fill d-flex flex-column p-3">
+          {title && <h1> {title} </h1>}
+
+          {breadcrumbs && (
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">Home</li>
+
+                {breadcrumbs.map((breadcrumb, index) => (
+                  <li key={index} className="breadcrumb-item">
+                    {breadcrumb}
+                  </li>
+                ))}
+              </ol>
+            </nav>
+          )}
+
           {children}
         </div>
       </div>
