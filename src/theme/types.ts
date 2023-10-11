acdbd21de8c73c3a85eb58dd87deba5155858097
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-11 17:14:36 -0300

    feat: add global types

---
+++ src/theme/types.ts
@@ -0,0 +1,3 @@
+export type TextColor = 'text-primary' | 'text-secondary' | 'text-success' | 'text-danger' | 'text-warning' | 'text-info' | 'text-light' | 'text-dark' | 'text-body' | 'text-black' | 'text-white' | 'text-black-50' | 'text-white-50';
+
+export type BackgroundColor = 'bg-primary' | 'bg-primary-subtle' | 'bg-secondary' | 'bg-secondary-subtle' | 'bg-success' | 'bg-success-subtle' | 'bg-danger' | 'bg-danger-subtle' | 'bg-warning' | 'bg-warning-subtle' | 'bg-info' | 'bg-info-subtle' | 'bg-light' | 'bg-light-subtle' | 'bg-dark' | 'bg-dark-subtle' | 'bg-body-secondary' | 'bg-body-tertiary' | 'bg-body' | 'bg-black' | 'bg-white' | 'bg-transparent';
