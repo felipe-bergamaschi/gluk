e5ca462d1aa1606b83e1f9c098b35ac1dc30f45c
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-12 10:16:05 -0300

    feat: add body

---
+++ src/components/layouts/common/body/index.tsx
@@ -0,0 +1,28 @@
+export interface BodyProps {
+  title: string;
+  breadcrumbs?: string[];
+  children: React.ReactNode
+}
+
+export function Body({ children, title, breadcrumbs }: BodyProps) {
+  return (
+    <div className="container d-flex flex-column p-3">
+      <h2> {title} </h2>
+
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">Home</li>
+          <li className="breadcrumb-item">{title}</li>
+
+          {breadcrumbs?.map((breadcrumb, index) => (
+            <li key={index} className="breadcrumb-item">
+              {breadcrumb}
+            </li>
+          ))}
+        </ol>
+      </nav>
+
+      {children}
+    </div>
+  )
+}
