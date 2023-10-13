4f45f8d51ad888f7e94fb102e789142678656a45
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-13 15:02:33 -0300

    fix: kita config

--- backend/kita.config.js
+++ backend/kita.config.js
@@ -4,17 +4,9 @@ const path = require('path');
 module.exports = {
   tsconfig: require.resolve('./tsconfig.json'),
   routes: {
-    // format: require('../../.prettierrc.js'),
     output: path.resolve(__dirname, './src/routes.ts'),
 
-    // for tests to work
     exportControllers: true
-  },
-  params: {
-    // FarmDay: [path.resolve(__dirname, './src/params/farm-day'), { schemaTransformer: true }],
-    // Authorized: [path.resolve(__dirname, './src/params/authorized'), { schemaTransformer: true }],
-    // FileUpload: [path.resolve(__dirname, './src/params/file-upload'), { schemaTransformer: true }],
-    // FileStream: [path.resolve(__dirname, './src/params/file-stream'), { schemaTransformer: true }]
   },
   schema: {
     defaultResponse: '2xx',
