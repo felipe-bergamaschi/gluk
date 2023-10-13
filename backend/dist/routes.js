b69f7eab4682dc9f80e6f5ec765bb3e8ac6a199e
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-13 15:17:39 -0300

    fix: dist

--- backend/dist/routes.js
+++ backend/dist/routes.js
@@ -1,10 +1,11 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsersController = exports.HealthcheckController = exports.Kita = void 0;
+exports.UsersController = exports.ProductsController = exports.HealthcheckController = exports.Kita = void 0;
 const tslib_1 = require("tslib");
 const fastify_plugin_1 = tslib_1.__importDefault(require("fastify-plugin"));
 require("@fastify/swagger");
 const HealthcheckController = tslib_1.__importStar(require("./routes/healthcheck"));
+const ProductsController = tslib_1.__importStar(require("./routes/products"));
 const UsersController = tslib_1.__importStar(require("./routes/users"));
 exports.Kita = (0, fastify_plugin_1.default)(async (fastify, options) => {
     fastify.addSchema({
@@ -25,19 +26,55 @@ exports.Kita = (0, fastify_plugin_1.default)(async (fastify, options) => {
         additionalProperties: false,
     });
     fastify.addSchema({
+        $id: "ProductsControllerGetResponse",
+        type: "array",
+        items: {
+            type: "object",
+            properties: {
+                id: {
+                    type: "string",
+                },
+                name: {
+                    type: "string",
+                },
+                price: {
+                    type: "number",
+                },
+                images: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            id: {
+                                type: "string",
+                            },
+                            url: {
+                                type: "string",
+                            },
+                        },
+                        required: ["id", "url"],
+                        additionalProperties: false,
+                    },
+                },
+            },
+            required: ["id", "name", "price", "images"],
+            additionalProperties: false,
+        },
+    });
+    fastify.addSchema({
         $id: "UsersControllerGetResponse",
         type: "array",
         items: {
             type: "object",
             properties: {
+                id: {
+                    type: "number",
+                },
                 name: {
                     type: "string",
                 },
-                id: {
-                    type: "string",
-                },
             },
-            required: ["name", "id"],
+            required: ["id", "name"],
             additionalProperties: false,
         },
     });
@@ -53,6 +90,19 @@ exports.Kita = (0, fastify_plugin_1.default)(async (fastify, options) => {
         },
     }, async (request, reply) => {
         return HealthcheckController.get();
+    });
+    fastify.get("/products", {
+        schema: {
+            operationId: "products",
+            response: {
+                "2xx": { $ref: "ProductsControllerGetResponse" },
+                "4xx": { $ref: "ErrorResponse" },
+                "5xx": { $ref: "ErrorResponse" },
+            },
+            tags: ["products"],
+        },
+    }, async (request, reply) => {
+        return ProductsController.get();
     });
     fastify.get("/users", {
         schema: {
@@ -73,5 +123,6 @@ exports.Kita = (0, fastify_plugin_1.default)(async (fastify, options) => {
     encapsulate: true,
 });
 exports.HealthcheckController = tslib_1.__importStar(require("./routes/healthcheck"));
+exports.ProductsController = tslib_1.__importStar(require("./routes/products"));
 exports.UsersController = tslib_1.__importStar(require("./routes/users"));
 //# sourceMappingURL=routes.js.map
--- backend/dist/routes/users.js
+++ backend/dist/routes/users.js
@@ -3,14 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.get = void 0;
 async function get() {
     return [
-        {
-            "name": "Felipe Bergamaschi",
-            "id": "clnm0m6ih3oxl01uid1nzbwi3"
-        },
-        {
-            "name": "Public",
-            "id": "PUBLIC"
-        }
+        { id: 1, name: "Alice" },
+        { id: 2, name: "Bob" },
+        { id: 3, name: "Charlie" },
+        { id: 4, name: "David" },
+        { id: 5, name: "Eve" },
+        { id: 6, name: "Frank" },
+        { id: 7, name: "Grace" },
+        { id: 8, name: "Helen" },
+        { id: 9, name: "Isaac" },
+        { id: 10, name: "Jane" },
+        { id: 11, name: "Kevin" },
+        { id: 12, name: "Linda" },
+        { id: 13, name: "Mike" },
+        { id: 14, name: "Nancy" },
+        { id: 15, name: "Oliver" },
+        { id: 16, name: "Pamela" },
+        { id: 17, name: "Quincy" },
+        { id: 18, name: "Rita" },
+        { id: 19, name: "Steve" },
+        { id: 20, name: "Tina" }
     ];
 }
 exports.get = get;
