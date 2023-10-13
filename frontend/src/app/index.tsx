96ce4daa2bb486ca5092a1ba74b0104b664757b2
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-13 18:41:22 -0300

    feat: add context

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,20 +1,23 @@
 import { OrderDetails } from '@/features/orderDetails'
 import { CommonLayout } from '../components/layouts/common'
 import { ProductContent } from '@/features/productContent'
+import { Provider as ProductProvider } from '@/contexts/products'
 
 export default function Page() {
   return (
-    <CommonLayout
-      title='Nova venda'
-      breadcrumbs={['Cadastro de venda']}
-      sidebar={{
-        title: 'Detalhes da venda',
-        content: <OrderDetails />,
-        open: true,
-        hideCloseButton: true,
-      }}
-    >
-      <ProductContent />
-    </CommonLayout>
+    <ProductProvider>
+      <CommonLayout
+        title='Nova venda'
+        breadcrumbs={['Cadastro de venda']}
+        sidebar={{
+          title: 'Detalhes da venda',
+          content: <OrderDetails />,
+          open: true,
+          hideCloseButton: true,
+        }}
+      >
+        <ProductContent />
+      </CommonLayout>
+    </ProductProvider>
   )
 }
---
+++ frontend/src/contexts/products.tsx
@@ -0,0 +1,45 @@
+import { ProductsControllerGetResponseItem } from '@/query';
+import type { ReactNode } from 'react';
+import { useCallback, useState } from 'react';
+
+import { createContext as createContextSelector, useContextSelector } from 'use-context-selector';
+
+type ContextData = {
+  products: ProductsControllerGetResponseItem[] | null;
+  handleSetProducts: (data: ProductsControllerGetResponseItem) => void;
+};
+
+type ProviderProps = {
+  children: ReactNode;
+};
+
+export const Context = createContextSelector({} as ContextData);
+
+export function Provider(props: ProviderProps) {
+  const [products, setProducts] = useState<ProductsControllerGetResponseItem[] | null>(null);
+
+  const handleSetProducts = useCallback((data: ProductsControllerGetResponseItem) => {
+    setProducts((prev) => (prev ? [...prev, data] : [data]))
+  }, []);
+
+  return (
+    <Context.Provider
+      value={{
+        products,
+        handleSetProducts
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+}
+
+export function useListProducts() {
+  const products = useContextSelector(Context, (context) => context.products);
+  const handleSetProducts = useContextSelector(Context, (context) => context.handleSetProducts);
+
+  return {
+    products,
+    handleSetProducts
+  };
+}
