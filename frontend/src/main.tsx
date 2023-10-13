45c5ae7b42c133777d0c314fd34639d471612c6e
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-13 18:41:08 -0300

    feat: add react hot toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
 
 import { AppRoutes } from './routes'
 
@@ -15,6 +16,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppRoutes />
+
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
     </QueryClientProvider>
   </React.StrictMode>,
 )
