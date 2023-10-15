95b613fc076a3fa3cc4622adc8ebde13b68b8208
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-15 17:43:51 -0300

    fix: text field

--- frontend/src/components/form/textField.tsx
+++ frontend/src/components/form/textField.tsx
@@ -1,31 +1,42 @@
+import { useState } from 'react';
 import { useForm } from './container';
 
 interface TextFieldProps {
   name: string;
   label: string;
   placeholder?: string;
+  defaultValue?: string;
+  type?: string;
   onChange?: (e: any) => void;
+  onChangeMask?: (value: string) => void;
 }
 
-export function TextField({ name, label, placeholder, onChange }: TextFieldProps) {
+export function TextField({ name, label, placeholder, onChange, defaultValue, onChangeMask, type }: TextFieldProps) {
   const { formik } = useForm();
+
+  const [value, setValue] = useState(defaultValue ?? formik.values[name]);
 
   function handleChange(e: any) {
     formik.handleChange(e);
-    onChange && onChange(e);
+
+    const formattedValue = onChangeMask && onChangeMask(e.target.value);
+
+    setValue(formattedValue ?? e.target.value);
+
+    onChange && onChange(formattedValue ?? e);
   }
 
   return (
     <div className="input-group mb-3">
       <div className="form-floating">
         <input
-          type="text"
+          type={type ?? "text"}
           className="form-control"
           id={name}
           placeholder={placeholder}
           name={name}
           onChange={handleChange}
-          value={formik.values[name]}
+          value={value}
         />
 
         <label htmlFor={name}>{label}</label>
