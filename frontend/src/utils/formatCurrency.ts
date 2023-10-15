c129cbab1cb0f14eccb809b990cc18b31e738ea8
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-15 17:43:36 -0300

    feat: add format currency

--- frontend/src/utils/formatCurrency.ts
+++ frontend/src/utils/formatCurrency.ts
@@ -1,5 +1,13 @@
 import Decimal from "decimal.js";
 
+function sanitizeValue(value: string): string {
+  return value
+    .split('')
+    .filter((s: any) => /\d/.test(s))
+    .join('')
+    .padStart(3, '0');
+}
+
 export function formatCurrency(
   value: string | number,
   decimalScale?: number
@@ -16,4 +24,30 @@ export function formatCurrency(
     style: 'currency',
     currency: 'BRL'
   }).format(formattedValue)
+}
+
+export function changeFormatCurrency(value: string) {
+  const formattedValue = sanitizeValue(value)
+
+  const sanitizedValue = `${formattedValue.slice(0, -2)}.${formattedValue.slice(-2)}`;
+
+  const decimalValue = new Decimal(sanitizedValue);
+  const formatValue = decimalValue.toDecimalPlaces(2, Decimal.ROUND_DOWN).toNumber();
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(formatValue);
+}
+
+export function formatCurrencySubmit(value: string) {
+  const formattedValue = value
+    .replace('R$', '')
+    .replace(/\s/g, '')
+    .replace(/\./g, '')
+    .replace(/,/g, '.');
+
+  const decimalValue = new Decimal(formattedValue);
+
+  return decimalValue.toDecimalPlaces(2, Decimal.ROUND_DOWN).toNumber();
 }
