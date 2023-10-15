9269d9201f7955e4b6a4fd592e689303c6c2adcd
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-10-15 19:27:13 -0300

    fix: dist

---
+++ backend/dist/routes/create/order.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.get = void 0;
+const fs = require('fs');
+const path = require('path');
+async function get() {
+    const filePath = path.join(__dirname, '../../db/order.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo JSON:', err);
+            return;
+        }
+        const jsonData = JSON.parse(data);
+        const novoObjeto = {
+            nome: 'NovoNome',
+            id: 1234
+        };
+        jsonData.push(novoObjeto);
+        const novoJSON = JSON.stringify(jsonData, null, 2);
+        console.log({ novoJSON });
+    });
+    return filePath;
+}
+exports.get = get;
+//# sourceMappingURL=order.js.map
---
+++ backend/dist/routes/create/products.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.get = void 0;
+const products_1 = require("../services/mock/products");
+async function get() {
+    return products_1.products;
+}
+exports.get = get;
+//# sourceMappingURL=products.js.map
